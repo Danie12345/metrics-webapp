5167c2fc7322be77ece3ce960afdb333f1854290
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-20 13:56:56 -0500

    Add all get methods

--- src/services/SomeService.js
+++ src/services/SomeService.js
@@ -1,9 +1,17 @@
-import http from '../http-common';
+import {
+  airPollution, airPollutionKey, openCageData, openCageDataKey, getCountries,
+} from '../http-common';
 
-const getStuff = () => http.get('/');
+const getCurrentPollutionData = (lat, lon) => airPollution.get(`/?lat=${lat}&lon=${lon}&appid=${airPollutionKey}`);
 
-const SomeService = {
-  getStuff,
+const getCoordsByCountry = (country) => openCageData.get(`/?key=${openCageDataKey}&q=${country}&pretty=1&no_annotations=1`);
+
+const getContinentCountries = (continent) => getCountries.get(`?region=${continent}&pretty=true`);
+
+const WorldService = {
+  getCurrentPollutionData,
+  getCoordsByCountry,
+  getContinentCountries,
 };
 
-export default SomeService;
+export default WorldService;
