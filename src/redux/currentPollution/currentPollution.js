dd782f6acc4348dec047420e7656af0e24537828
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-20 14:41:04 -0500

    Add pollution action makers
    Added get pollution and clear actions.
    Added sample response.

--- src/redux/currentPollution/currentPollution.js
+++ src/redux/currentPollution/currentPollution.js
@@ -1,28 +1,56 @@
 import WorldService from '../../services/WorldService';
 
-const GET_POLLUTION = 'template/template-reducer/GET_POLLUTION';
+const GET_POLLUTION = 'metrics/pollution/GET_POLLUTION';
+const CLEAR = 'metrics/pollution/CLEAR'
 
 async function getCurrentPollution(dispatch, getState) {
   const { pollution: currentData } = getState();
 
   if (Object.keys(currentData).length === 0) {
     const { data } = await WorldService.getCurrentPollutionData();
-    const fetchedCurrentPollution = data.map((stuff) => ({
-      fetchedValue: stuff.value,
-      default: 'default value',
-    }));
-
-    dispatch({ type: SOME_ACTION, payload: fetchedCurrentPollution });
+  
+    dispatch({ type: GET_POLLUTION, payload: data });
   }
+}
+
+function removeData(dispatch) {
+  dispatch({ type: CLEAR });
 }
 
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case SOME_ACTION:
-      return state;
+    case GET_POLLUTION:
+      return [action.payload];
+    case CLEAR:
+      return [];
     default:
       return state;
   }
 }
 
-export { getCurrentPollution };
+export { getCurrentPollution, removeData };
+
+// {
+//   "coord": [
+//     50.0,
+//     50.0
+//   ],
+//   "list": [
+//     {
+//       "dt": 1606147200,
+//       "main": {
+//         "aqi": 4.0
+//       },
+//       "components": {
+//         "co": 203.609,
+//         "no": 0.0,
+//         "no2": 0.396,
+//         "o3": 75.102,
+//         "so2": 0.648,
+//         "pm2_5": 23.253,
+//         "pm10": 92.214,
+//         "nh3": 0.117
+//       }
+//     }
+//   ]
+// }
