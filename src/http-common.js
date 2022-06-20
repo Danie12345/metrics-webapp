5d2df9983a1eec9c5cb30490dec9eb84caffaa97
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-20 15:25:25 -0500

    Change countries api

--- src/http-common.js
+++ src/http-common.js
@@ -4,7 +4,7 @@ const airPollutionURL = 'http://api.openweathermap.org/data/2.5/air_pollution';
 const airPollutionKey = 'aafa8bdb6721fd796e15393dd7b376bf';
 const openCageDataURL = 'https://api.opencagedata.com/geocode/v1/json';
 const openCageDataKey = 'ae1b0f54d8f8479c9d08f45eeec144a2';
-const getCountriesURL = 'https://api.first.org/data/v1/countries';
+const getCountriesURL = 'https://restcountries.com/v3.1';
 
 const airPollution = axios.create({
   baseURL: airPollutionURL,
--- src/services/WorldService.js
+++ src/services/WorldService.js
@@ -6,7 +6,7 @@ const getCurrentPollutionData = (lat, lon) => airPollution.get(`/?lat=${lat}&lon
 
 const getCoordsByCountry = (country) => openCageData.get(`/?key=${openCageDataKey}&q=${country}&pretty=1&no_annotations=1&limit=1`);
 
-const getContinentCountries = (continent) => getCountries.get(`?region=${continent}&pretty=true`);
+const getContinentCountries = (continent) => getCountries.get(`/region/${continent}`);
 
 const WorldService = {
   getCurrentPollutionData,
