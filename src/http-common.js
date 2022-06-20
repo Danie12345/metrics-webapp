de96574f0aca56f819dba0ee571c76efa162e5de
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-20 15:49:13 -0500

    Remove country coords

--- src/http-common.js
+++ src/http-common.js
@@ -2,19 +2,10 @@ import axios from 'axios';
 
 const airPollutionURL = 'http://api.openweathermap.org/data/2.5/air_pollution';
 const airPollutionKey = 'aafa8bdb6721fd796e15393dd7b376bf';
-const openCageDataURL = 'https://api.opencagedata.com/geocode/v1/json';
-const openCageDataKey = 'ae1b0f54d8f8479c9d08f45eeec144a2';
 const getCountriesURL = 'https://restcountries.com/v3.1';
 
 const airPollution = axios.create({
   baseURL: airPollutionURL,
-  headers: {
-    'Content-type': 'application/json',
-  },
-});
-
-const openCageData = axios.create({
-  baseURL: openCageDataURL,
   headers: {
     'Content-type': 'application/json',
   },
@@ -28,5 +19,5 @@ const getCountries = axios.create({
 });
 
 export {
-  airPollution, airPollutionKey, openCageData, openCageDataKey, getCountries,
+  airPollution, airPollutionKey, getCountries,
 };
--- src/services/WorldService.js
+++ src/services/WorldService.js
@@ -1,16 +1,13 @@
 import {
-  airPollution, airPollutionKey, openCageData, openCageDataKey, getCountries,
+  airPollution, airPollutionKey, getCountries,
 } from '../http-common';
 
 const getCurrentPollutionData = (lat, lon) => airPollution.get(`/?lat=${lat}&lon=${lon}&appid=${airPollutionKey}`);
-
-const getCoordsByCountry = (country) => openCageData.get(`/?key=${openCageDataKey}&q=${country}&pretty=1&no_annotations=1&limit=1`);
 
 const getContinentCountries = (continent) => getCountries.get(`/region/${continent}`);
 
 const WorldService = {
   getCurrentPollutionData,
-  getCoordsByCountry,
   getContinentCountries,
 };
 
