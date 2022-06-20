45fac07a2ea53955c0daf0bfc9a99e51bf5b3b15
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-20 13:13:27 -0500

    Add api urls and keys

--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,22 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://';
+const airPollution_URL = 'http://api.openweathermap.org/data/2.5/';
+const airPollution_key = 'aafa8bdb6721fd796e15393dd7b376bf';
+const openCageData_URL = 'https://api.opencagedata.com/geocode/v1/';
+const openCageData_key = 'ae1b0f54d8f8479c9d08f45eeec144a2';
 
-export default axios.create({
-  baseURL: BASE_URL,
+const airPollution = axios.create({
+  baseURL: airPollution_URL,
   headers: {
     'Content-type': 'application/json',
   },
 });
+
+const openCageData = axios.create({
+  baseURL: openCageData_URL,
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+export { airPollution, airPollution_key, openCageData, openCageData_key };
