8d45bff119b3a170c4ac1e535826450f1dfd1c40
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-20 15:47:05 -0500

    Add countries per continent

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import pollution from './currentPollution/currentPollution';
+import countries from './continentCountries/continentCountries';
 
 const rootReducer = combineReducers({
   pollution,
+  countries,
 });
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
