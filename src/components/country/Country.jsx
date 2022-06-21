e1032d89b4134aeab233a3d885ccff56295f3179
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-21 14:13:57 -0500

    Fix eslint errors

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { setCountry } from '../../redux/country/country';
--- src/redux/country/country.js
+++ src/redux/country/country.js
@@ -1,14 +1,14 @@
 const SET_COUNTRY = 'metrics/country/SET_COUNTRY';
 
 function setCountry(dispatch, country) {
-  let payload = country;
+  const payload = country;
   if (Object.keys(country).length === 0) {
     payload.name = '';
   }
-  dispatch({ type: SET_COUNTRY, payload: payload });
+  dispatch({ type: SET_COUNTRY, payload });
 }
 
-export default function reducer(state = {name: ''}, action) {
+export default function reducer(state = { name: '' }, action) {
   switch (action.type) {
     case SET_COUNTRY:
       return action.payload;
