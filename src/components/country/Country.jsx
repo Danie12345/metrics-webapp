e92a7252bd7de8b9cdeb7a331176fbe5043e7b7a
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-21 11:29:21 -0500

    Add back button

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,9 +1,23 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { setCountry } from '../../redux/country/country';
 
 const Country = (props) => {
+  const dispatch = useDispatch();
+  const countrySelect = (country) => {
+    setCountry(dispatch, country);
+  };
   const { name } = props;
   return (
     <div className="country">
+      <NavLink
+        to="/continents"
+        className="nav-link"
+        onClick={() => { countrySelect(''); }}
+      >
+        <span>BACK</span>
+      </NavLink>
       {name}
     </div>
   );
