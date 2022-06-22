f917e030c63ae79f51a6fb8c552574e415bb9fb5
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-22 15:26:07 -0500

    Add status and update back link

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -2,16 +2,28 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { setCountry } from '../../redux/country/country';
-import { getCurrentPollution } from '../../redux/currentPollution/currentPollution';
+import { getCurrentPollution, removePollutionData } from '../../redux/currentPollution/currentPollution';
 
 import './Country.css';
 
 const Country = () => {
   const dispatch = useDispatch();
+  const continent = useSelector((state) => state.continent);
   const country = useSelector((state) => state.country);
   const pollution = useSelector((state) => state.pollution);
   const countrySelect = (country) => {
     setCountry(dispatch, country);
+  };
+  const back = () => {
+    countrySelect({});
+    removePollutionData(dispatch);
+  };
+  const status = {
+    1: ['Good', '#0f0'],
+    2: ['Moderate', '#66ff00'],
+    3: ['Unhealthy', '#eeff00'],
+    4: ['Very Unhealthy', '#ff7300'],
+    5: ['Hazardous', '#ff2600'],
   };
 
   useEffect(() => {
@@ -23,27 +35,28 @@ const Country = () => {
       <div>
         <NavLink
           to="/continents"
-          onClick={() => { countrySelect({}); }}
+          onClick={back}
+          className="back-link"
         >
-          <span>BACK</span>
+          <span>{`Back to ${continent}`}</span>
         </NavLink>
+        <span className="country-path">{` > ${country.name}`}</span>
       </div>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <span>{country.name}</span>
-        <span>{country.region}</span>
-        <span>{country.coords[0]}</span>
-        <span>{country.coords[1]}</span>
         <span>{`Population: ${country.population}`}</span>
         <img style={{ width: '120px', height: 'auto' }} alt={`${country.name}'s flag.`} src={country.flag} />
         {pollution && Object.keys(pollution).length > 0
           ? (
             <div>
               <div>
-                <span>{pollution.lat}</span>
-                <span>{pollution.lon}</span>
-              </div>
-              <div>
-                <span>{pollution.index}</span>
+                <span
+                  style={{
+                    color: status[pollution.index][1],
+                  }}
+                >
+                  {pollution.index}
+                </span>
                 {Object.keys(pollution.composition).map((key) => (
                   <div key={key}>
                     <span>{key}</span>
